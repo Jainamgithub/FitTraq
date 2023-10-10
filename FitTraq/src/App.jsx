31cab8799292157126b7082c1f2a085d37f702bf
Jainam sheth <g21it.jainam.sheth@gnkhalsa.edu.in>
2023-10-10 18:17:52 +0530

    initial commit

---
+++ FitTraq/src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import LogFull from "../src/FullPages/LogFull";
+export default function App() {
+  
+  return (
+    <div className="App">
+      <LogFull/>
+
+     </div>        
+  );
+}
